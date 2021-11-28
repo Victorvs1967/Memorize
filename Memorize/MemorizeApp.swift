8d55169a6ee9034e2018c04effb992d82d82a63e
Victor <victorsmirnov@me.com>
2021-11-28 20:35:20 +0200

    renew app

--- Memorize/MemorizeApp.swift
+++ Memorize/MemorizeApp.swift
@@ -2,16 +2,17 @@
 //  MemorizeApp.swift
 //  Memorize
 //
-//  Created by Victor Smirnov on 28.11.2021.
+//  Created by Victor Smirnov on 07.11.2021.
 //
 
 import SwiftUI
 
 @main
 struct MemorizeApp: App {
+    private let game = EmojiMemoryGame()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            EmojiMemoryGameView(game: game)
         }
     }
 }
