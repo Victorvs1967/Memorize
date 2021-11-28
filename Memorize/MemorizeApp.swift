db51d9e16d1f4ff11f3710f9828af8d8d00516c8
Victor <victorsmirnov@me.com>
2021-11-28 20:23:51 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Victor Smirnov on 28.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
